f31fd31caa0ed6777eab2830d1c8f1ef06de3519
vany2009 <phanthilieu11071983@gmail.com>
2025-02-17 17:00:18 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai11_Digitalclock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void Timeri_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm:ss");
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+timer1.Start();
+        }
+    }
+}
